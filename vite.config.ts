e13c741e9873e42c95c8e5916b37a1abdc29a921
Evgen <zensot1@yandex.ru>
2025-05-17 22:04:22 +0500

    хосты в переменной окружения

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite';
+/// <reference types="node" />
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const API = env.SERVER_API_URL;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: API,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (p) => p.replace(/^\/api/, ""),
+        },
       },
+      host: "0.0.0.0",
+      port: 5173,
     },
-  },
-})
+  };
+});
